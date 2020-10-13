637106baa85ec28b41812e7c629272865e20e1e4
Svinka22 <58908173+Svinka22@users.noreply.github.com>
2020-10-13 08:16:40 +0300

    function arr.map

--- src/Components/Context/Post/PostDiv.jsx
+++ src/Components/Context/Post/PostDiv.jsx
@@ -3,10 +3,17 @@ import PostDivStyle from './PostDiv.module.css';
 import Post from './Post'
 
 const PostDiv = () => {
+
+    const posts = [
+        { imgLogoPost: 'https://image.flaticon.com/icons/svg/145/145859.svg', text: 'I have many problem))', like: '43'},
+        { imgLogoPost: 'https://image.flaticon.com/icons/svg/145/145867.svg', text: 'I too bro))', like: '36'}
+    ];
+
+    const post = posts.map( element => <Post imgLogoPost={element.imgLogoPost} text={element.text} like={element.like} /> )
+
     return (<div className={PostDivStyle.PostDiv}>
 
-        <Post imgLogoPost='https://image.flaticon.com/icons/svg/145/145859.svg' text='I have many problem))' like={0} />
-        <Post imgLogoPost='https://image.flaticon.com/icons/svg/145/145867.svg' text='I too bro))' like={0} />
+        { post }
 
     </div>);
 };
--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -6,13 +6,13 @@ const Dialog = (props) => {
     return (<div className={MessageStyle.userMessageLeft}>
         <div className={MessageStyle.img}>
             <img src={props.img} className={MessageStyle.avotarky} />
-        </div>                
+        </div>
         <div className={MessageStyle.spanBort}>{props.text}</div>
     </div>);
 };
 
 const Friends = (props) => {
-    return(<div className={MessageStyle.dialog} >
+    return (<div className={MessageStyle.dialog} >
         <li>
             <NavLink activeClassName={MessageStyle.size} to={props.userId}><b>{props.userName}</b></NavLink>
         </li>
@@ -24,19 +24,25 @@ let noNameImg = 'https://www.flaticon.com/svg/static/icons/svg/848/848043.svg'
 const Message = (props) => {
 
     const friends = [
-        { id : '/Message/1', name : 'Victor' },
-        { id : '/Message/2', name : 'Anton' },
-        { id : '/Message/3', name : 'Anna' },
-        { id : '/Message/4', name : 'Vika' }
+        { id: '/Message/1', name: 'Victor' },
+        { id: '/Message/2', name: 'Anton' },
+        { id: '/Message/3', name: 'Anna' },
+        { id: '/Message/4', name: 'Vika' },
+        { id: '/Message/5', name: 'Alica' },
+        { id: '/Message/6', name: 'Sveta' }
     ];
 
+    const friendsElements = friends.map( element => <Friends userName={element.name} userId={element.id}/> );
+
     const messages = [
-        { url : 'https://image.flaticon.com/icons/svg/145/145859.svg', text : "Hello, Anton!!! How are you? Where are you bro?" },
-        { url : 'https://image.flaticon.com/icons/svg/145/145867.svg', text : "I'm ok)) How are you bro? I think that your WEBSITE was finished?" },
-        { url : 'https://image.flaticon.com/icons/svg/145/145859.svg', text : "Yes! It is very difecalt, but i could do it! I'm very happy now))" },
-        { url : 'https://image.flaticon.com/icons/svg/145/145867.svg', text : "Oh, sorry, I haven't free time, I must rut to my course)) See you bro))" },
-        { url : 'https://image.flaticon.com/icons/svg/145/145859.svg', text : "See you bro)) Goodby!!!" },
+        { url: 'https://image.flaticon.com/icons/svg/145/145859.svg', text: "Hello, Anton!!! How are you? Where are you bro?" },
+        { url: 'https://image.flaticon.com/icons/svg/145/145867.svg', text: "I'm ok)) How are you bro? I think that your WEBSITE was finished?" },
+        { url: 'https://image.flaticon.com/icons/svg/145/145859.svg', text: "Yes! It is very difecalt, but i could do it! I'm very happy now))" },
+        { url: 'https://image.flaticon.com/icons/svg/145/145867.svg', text: "Oh, sorry, I haven't free time, I must rut to my course)) See you bro))" },
+        { url: 'https://image.flaticon.com/icons/svg/145/145859.svg', text: "See you bro)) Goodby!!!" }
     ];
+
+    const messagesElements = messages.map( elememnt => <Dialog img={elememnt.url} text={elememnt.text} /> );
 
     return (<div className={MessageStyle.MessageMainDiv}>
 
@@ -44,17 +50,10 @@ const Message = (props) => {
             DIALOGS
         </div>
         <div className={MessageStyle.friends}>
-            <Friends userName={friends[0].name} userId={friends[0].id}/>
-            <Friends userName={friends[1].name} userId={friends[1].id}/>
-            <Friends userName={friends[2].name} userId={friends[2].id}/>
-            <Friends userName={friends[3].name} userId={friends[3].id}/>
+            { friendsElements }
         </div>
         <div className={MessageStyle.messages}>
-            <Dialog img={messages[0].url} text={messages[0].text}/>
-            <Dialog img={messages[1].url} text={messages[1].text}/>
-            <Dialog img={messages[2].url} text={messages[2].text}/>
-            <Dialog img={messages[3].url} text={messages[3].text}/>
-            <Dialog img={messages[4].url} text={messages[4].text}/>
+            { messagesElements }
         </div>
 
     </div>);
